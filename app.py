5aae295b9b8b07a35bf1676543cf3febf3796c90
David Smiles <ugberodavid@gmail.com>
2021-06-30 16:35:43 +0100

    omooo.. alot of changes

--- app.py
+++ app.py
@@ -1,9 +1,19 @@
 import requests
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, redirect, url_for
 
 app = Flask(__name__)
 
 uri = 'http://localhost:5000'
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/admin')
+def index_admin():
+    return render_template('admin.html')
 
 
 @app.route('/signup', methods=['GET', 'POST'])
@@ -11,11 +21,24 @@ def signup():
     if request.method == 'POST':
         url = f'{uri}/signup'
         data = dict(request.form)
+        print(data)
         response = requests.post(url, json=data)
         r_json = response.json()
-        return r_json
+        return redirect(url_for('verify', logged_in_as='user'))
 
     return render_template('signup.html')
+
+
+@app.route('/admin/signup', methods=['GET', 'POST'])
+def adminsignup():
+    if request.method == 'POST':
+        url = f'{uri}/admin/signup'
+        data = dict(request.form)
+        response = requests.post(url, json=data)
+        r_json = response.json()
+        return redirect(url_for('verify', logged_in_as='admin'))
+
+    return render_template('adminsignup.html')
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -24,8 +47,12 @@ def login():
         url = f'{uri}/login'
         data = dict(request.form)
         response = requests.post(url, json=data)
-        r_json = response.json()
-        return r_json
+        if response.ok:
+            r_json = response.json()
+            logged_in_as = r_json['logged_in_as']
+            print(logged_in_as)
+            return redirect(url_for('verify', logged_in_as=logged_in_as))
+        print('unsuccessful')
 
     return render_template('login.html')
 
@@ -42,17 +69,22 @@ def sign():
     return render_template('sign.html')
 
 
-@app.route('/verify', methods=['GET', 'POST'])
-def verify():
+@app.route('/verify/<logged_in_as>', methods=['GET', 'POST'])
+def verify(logged_in_as):
     if request.method == 'POST':
-        url = f'{uri}/sign'
+        url = f'{uri}/verify'
         data = request.files
         response = requests.post(url, files=data)
         if response.ok:
             return render_template('valid.html')
         return render_template('invalid.html')
 
-    return render_template('verify.html')
+    return render_template('verify.html', logged_in_as=logged_in_as)
+
+
+@app.route('/upload')
+def upload():
+    return render_template('upload.html')
 
 
 if __name__ == '__main__':
